3b769f348ee88e10b17de75af956710cd2d361d9
moderatorman <12193288+moderatorman@users.noreply.github.com>
2021-04-14 20:48:03 -0700

    Bug fixes

--- src/main/java/net/oldschoolminecraft/pf/handlers/BlockHandler.java
+++ src/main/java/net/oldschoolminecraft/pf/handlers/BlockHandler.java
@@ -1,6 +1,8 @@
 package net.oldschoolminecraft.pf.handlers;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.Furnace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockListener;
@@ -13,9 +15,20 @@ public class BlockHandler extends BlockListener
     {
         if (event.getBlock().getType().equals(Material.BURNING_FURNACE))
         {
+            if (!isEmpty(((Furnace)event.getBlock().getState()).getInventory().getContents()))
+                return;
+
             event.getPlayer().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.BURNING_FURNACE, 1));
             event.getBlock().setType(Material.AIR);
             event.setCancelled(true);
         }
+    }
+
+    private boolean isEmpty(ItemStack[] inv)
+    {
+        for (ItemStack stack : inv)
+            if (stack != null)
+                return false;
+        return true;
     }
 }
--- src/main/java/net/oldschoolminecraft/pf/handlers/PlayerHandler.java
+++ src/main/java/net/oldschoolminecraft/pf/handlers/PlayerHandler.java
@@ -1,18 +1,28 @@
 package net.oldschoolminecraft.pf.handlers;
 
 import org.bukkit.Material;
+import org.bukkit.block.Furnace;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerHandler extends PlayerListener
 {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event)
     {
-        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.BURNING_FURNACE))
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.BURNING_FURNACE) && isEmpty(((Furnace)event.getClickedBlock().getState()).getInventory().getContents()))
             event.setUseInteractedBlock(Event.Result.DENY);
+    }
+
+    private boolean isEmpty(ItemStack[] inv)
+    {
+        for (ItemStack stack : inv)
+            if (stack != null)
+                return false;
+        return true;
     }
 }
